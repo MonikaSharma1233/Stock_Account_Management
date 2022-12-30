ca5c6f23ef5238b796ee9ef9527dd15a1f2a9fc1
firsttoken <monikasharma12330@gmail.com>
2022-12-30 22:05:42 +0530

    Fatching data from json file

---
+++ FatchStockAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace Stock_Account_Management
+{
+    public class FatchStockAccount
+    {
+        
+        public Model_class Read(string path)
+        {
+
+
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Model_class>(json);
+                }
+
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+        }
+        
+
+
+    }
+}
